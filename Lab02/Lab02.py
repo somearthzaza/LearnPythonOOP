64d90b2ed2bb3bf7aef78057b5db418b0292fde7
9Chinz <MR.Chinnapat@hotmail.com>
2022-01-28 16:39:30 +0700

    add validation author

--- Lab02/Lab02.py
+++ Lab02/Lab02.py
@@ -18,12 +18,20 @@ class Catalog:
 
 
 class Author:
+
     def __init__(self, name):
         self._name = name
 
     @property
     def name(self):
         return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str):
+            self._name = name
+        else:
+            print("String only")
 
 
 class Book:
